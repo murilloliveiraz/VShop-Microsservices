c60c5a5655b528b817670690762245437984b724
Murillo Oliveira <murillo1047@gmail.com>
2024-12-15 12:45:43 -0300

    feat: web application

---
+++ VShop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VShop.Web.Models;
+
+public class ProductViewModel
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public decimal price { get; set; }
+    public string? description { get; set; }
+    public long stock { get; set; }
+    public string? imageurl { get; set; }
+    public string? categoryName { get; set; }
+    public int categoryid { get; set; }
+}
